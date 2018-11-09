20753e96a2e16dfc79cbd96e643217c84f98434e
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-11-09 12:07:51 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#
+author = Author.create(name: "George Lucas")
+movie = Movie.create(title: "Star wars", author: author)
+movie2 = Movie.create(title: "Test film", author: author)
+user = User.create(username: "test", password: "123")
+review1 = Review.create(user: user, movie: movie, rating: 5, text: "test review 1")
+review2 = Review.create(user: user, movie: movie2, rating: 5, text: "test review 2")
+puts review2.errors.inspect
